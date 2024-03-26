e725be24d0293839fd682334e15b0c223c2791b5
Gavin Newell <grex326@gmail.com>
2024-03-26 01:35:30 -0400

    edited the main to do the logic instead of the templates due to repeated errors when I attempted to perform the operations outside of main.

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ def instructions():
 
 @app.route('/add/<num1>/<num2>')
 def addNums(num1, num2):
+    num3 = num1 + num2
     return render_template("addNums.html", numb1=num1, numb2=num2)
 
 @app.route('/sub/<num1>/<num2>')
 def subNums(num1, num2):
+    num3 = num1 - num2
     return render_template("subNums.html", numb1=num1, numb2=num2)
 
 @app.route('/mult/<num1>/<num2>')
 def multNums(num1, num2):
+    num3 = num1 * num2
     return render_template("multNums.html", numb1=num1, numb2=num2)
 
 @app.route('/div/<num1>/<num2>')
 def divNums(num1, num2):
+    num3 = num1 / num2
     return render_template("divNums.html", numb1=num1, numb2=num2)
