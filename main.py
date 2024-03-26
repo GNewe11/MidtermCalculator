c85ed6b1ea2e577b709079cbd7e2daff84b17cd4
Gavin Newell <grex326@gmail.com>
2024-03-26 01:42:41 -0400

    added numb3=num3 to the render_template parameters so it could print properly

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ def instructions():
 @app.route('/add/<num1>/<num2>')
 def addNums(num1, num2):
     num3 = int(num1) + int(num2)
-    return render_template("addNums.html", numb1=num1, numb2=num2)
+    return render_template("addNums.html", numb1=num1, numb2=num2, numb3=num3)
 
 @app.route('/sub/<num1>/<num2>')
 def subNums(num1, num2):
     num3 = int(num1) - int(num2)
-    return render_template("subNums.html", numb1=num1, numb2=num2)
+    return render_template("subNums.html", numb1=num1, numb2=num2, numb3=num3)
 
 @app.route('/mult/<num1>/<num2>')
 def multNums(num1, num2):
     num3 = int(num1) * int(num2)
-    return render_template("multNums.html", numb1=num1, numb2=num2)
+    return render_template("multNums.html", numb1=num1, numb2=num2, numb3=num3)
 
 @app.route('/div/<num1>/<num2>')
 def divNums(num1, num2):
     num3 = int(num1) / int(num2)
-    return render_template("divNums.html", numb1=num1, numb2=num2)
+    return render_template("divNums.html", numb1=num1, numb2=num2, numb3=num3)
